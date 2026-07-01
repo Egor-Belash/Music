01d0563ad96c94026fca3143fa65341df573aabb
Egor <egorbla228@gmail.com>
2026-07-01 11:11:22 +0300

    feat: add ErrorView

--- Music/TabBar/HomeTab/ErrorView.swift
+++ Music/TabBar/HomeTab/ErrorView.swift
@@ -5,4 +5,98 @@
 //  Created by Egor on 01.07.2026.
 //
 
-import Foundation
+import UIKit
+
+protocol ErrorViewDelegate: AnyObject {
+    func reloadButtonTapped()
+}
+
+final class ErrorView: UIView {
+    
+    // MARK: – Properties
+    weak var delegate: ErrorViewDelegate?
+    
+    // MARK: – Subviews
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Something went wrong"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .label
+        return label
+    }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Please try again"
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let retryButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Reload", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .greenColor2
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        button.layer.cornerRadius = 19
+        return button
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    // MARK: – INIT
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViewProperties()
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: – Layout
+    private func setupViewProperties() {
+        backgroundColor = .white
+        layer.cornerRadius = 10
+    }
+    
+    private func setupSubviews() {
+        addSubview(stackView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(messageLabel)
+        stackView.addArrangedSubview(retryButton)
+        retryButton.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            
+            retryButton.widthAnchor.constraint(equalToConstant: 80),
+            retryButton.heightAnchor.constraint(equalToConstant: 38),
+            
+        ])
+    }
+    
+    // MARK: – Actions
+    @objc private func retryButtonTapped() {
+        delegate?.reloadButtonTapped()
+    }
+}
